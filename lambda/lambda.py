82ea17e53410450b1b0bad3d361be634024a7a25
Norbert305 <sealsnorbert@yahoo.com>
2022-09-21 21:45:50 -0400

    lambda stuff

---
+++ lambda/lambda.py
@@ -0,0 +1,19 @@
+
+        #odd or even
+result = lambda arg : arg % 2 == 0
+
+print(result(2))
+
+
+        #sum values
+result_2 = lambda a,b : a + b
+
+print(result_2(5,5))
+
+
+        #multiply_result
+result_3 = lambda a,b : a * b
+
+print(result_3(5,2))
+
+
