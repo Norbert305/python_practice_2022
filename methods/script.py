0b5aefdbee072c5c00b102ee9f77f0b43e5623af
Norbert305 <sealsnorbert@yahoo.com>
2024-02-08 22:44:51 -0500

    tuples

--- methods/script.py
+++ methods/script.py
@@ -1,8 +1,13 @@
 
-# reverse list
+# reverse list of numbers
 my_list = [1,2,3,4,5]
 reversed_list = list(reversed(my_list))
 print(reversed_list) # [5, 4, 3, 2, 1]
+
+# reverse list of strings
+fruits = ['apple', 'orange', 'banana']
+x = list(reversed(fruits))
+print(x)
 
 # append list
 fruits = ['apple', 'banana', 'cherry']
---
+++ tuples/script.py
@@ -0,0 +1,16 @@
+my_tuple = ("red", "blue", "green")
+
+for data in my_tuple:
+    print(data)
+    
+    
+for i in range(len(my_tuple)):
+    print(my_tuple[i])
+    
+    
+print(my_tuple[-1])
+
+
+# The key difference between the tuples and lists is that while the tuples are immutable objects the lists are mutable. 
+# This means that tuples cannot be changed while the lists can be modified. 
+# Tuples are more memory efficient than the lists.
