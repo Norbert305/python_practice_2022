f006772cf316f2e9b90886bf6aa5a3d8a2a7db89
Norbert305 <sealsnorbert@yahoo.com>
2024-02-23 16:31:38 -0500

    more code

--- functions/functions.py
+++ functions/functions.py
@@ -24,3 +24,19 @@ print(times_this(5,2))
 subtract = lambda a,b: a - b
 
 print(subtract(20,10))
+
+
+def reverse_string(string):
+    
+    place_holder = ""
+    
+    string = string.split(' ')
+    
+    for i in range(len(string)-1,-1,-1):
+        place_holder += string[i]
+        place_holder += ' '
+        
+    return place_holder.strip()
+    
+print(reverse_string("Hello World"))
+    
