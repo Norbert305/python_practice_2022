3b78e81d86bab8e243d51ea0142f56cabfd82771
Norbert305 <sealsnorbert@yahoo.com>
2024-03-04 10:09:07 -0500

    concat and sort function

--- functions/concat.py
+++ functions/concat.py
@@ -5,3 +5,21 @@ def render_person(name, date, color, age, gender):
 
 # Do not edit below this line
 print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
+
+
+
+
+def sort_arrays(a,b):
+    
+    c = a + b
+    
+    my_list = []
+    
+    for data in range(len(c)):
+        if c[data] != 0:
+            my_list.append(c[data])
+            
+    return sorted(my_list)
+    
+    
+print(sort_arrays([2,3,1,5,0,0,0], [4,10,9,7,8,6]))
