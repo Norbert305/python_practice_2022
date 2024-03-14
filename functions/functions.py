1f74a2a93841f7851c54f4aaab7daee30e6166b2
Norbert305 <sealsnorbert@yahoo.com>
2024-03-14 16:51:22 -0400

    update

--- functions/functions.py
+++ functions/functions.py
@@ -69,5 +69,30 @@ def concat(name,age):
   return f"My name is {name} and I am {age} years old."
   
 print(concat("Bert",20))
+
+
+def find_duplicates(num):
+
+    obj = {}
+
+    for i in range(len(num)):
+        key = num[i]
+
+        if key in obj:
+                obj[key]+= 1
+            
+        else:
+            obj[key] = 1
+
+        
+    answer = []
+
+    for data in obj:
+        if obj[data] >= 2:
+            answer.append(data)
+
+    return answer
+
+print(find_duplicates([1,2,2,1,3,4,5,5])) # [1,2,5]
         
     
